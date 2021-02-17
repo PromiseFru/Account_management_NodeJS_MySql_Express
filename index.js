7a9546f0e4fcec830615295bd4a4915f676242bd
Promise Fru <promisefru1@gmail.com>
2021-02-17 14:20:42 +0100

    user auth i progress

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const routes = require('./routes/routes.js');
 port = 3000;
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    }
+}));
+
+app.use(passport.initialize());
 
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world working')
-});
+app.use('/', routes);
 
 app.listen(port, console.log(`Server listening on port ${port}`));
--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const router = express.Router();
+var Users = require('../models/User.js');
+
+let users = new Users();
+
+router.post('/user/auth/', async (req, res) => {
+    
+})
