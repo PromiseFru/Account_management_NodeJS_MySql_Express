1a657214cd4a59f0de2f40ad4fa5fdcf9627eefc
Promise Fru <promisefru1@gmail.com>
2021-03-06 10:35:31 +0100

    models assembled in index.js

---
+++ models/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+let db = {};
+
+let host = process.env.HOST;
+let user = process.env.OWNER;
+let databse = process.env.DATABASE;
+let password = process.env.PASSWORD;
+
+const sequelize = new Sequelize(databse, user, password, {
+    host: host,
+    dialect: "mysql"
+})
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.user = require('../models/user.model.js')(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "userId"
+});
+
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "roleId"
+});
+
+db.ROLES = ["admin", "user"];
+
+module.exports = db;
